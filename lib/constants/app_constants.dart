b8576f183dde64be0bc6bda79fc8618b942b448a
Beamlak Aschalew <birrletej12@gmail.com>
2025-10-30 18:12:27 +0300

    bump version

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -42,7 +42,7 @@ const kTextSmallAboutBodyStyle = TextStyle(
 const kTableLeftStyle =
     TextStyle(overflow: TextOverflow.ellipsis, fontWeight: FontWeight.bold);
 
-const String currentAppVersion = '2.7.2';
+const String currentAppVersion = '2.7.2-b2';
 
 final client = HttpClient();
 const retryOptions = RetryOptions(
